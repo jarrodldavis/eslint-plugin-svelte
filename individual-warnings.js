ac8450c2dd07b0e289dc133d75018876f480c0de
Jarrod Davis <developer@jarrodldavis.com>
2019-03-28 14:19:11 -0600

    Add compiler warnings postprocessor
    Add a separate processor plugin that separates reported compiler
    warnings into distinct subrules.

---
+++ individual-warnings.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const COMPILER_WARNING_RULE_ID = "@jarrodldavis/svelte/compiler-warnings";
+
+function preprocess(text) {
+  return [text];
+}
+
+function postprocess(problems) {
+  for (const problem of problems[0]) {
+    if (problem.ruleId !== COMPILER_WARNING_RULE_ID) {
+      continue;
+    }
+
+    const [compiler_code, compiler_message] = problem.message.split(": ");
+
+    problem.ruleId += `/${compiler_code}`;
+    problem.message = compiler_message;
+  }
+
+  return problems[0];
+}
+
+module.exports = {
+  processors: {
+    ".svelte": { preprocess, postprocess }
+  }
+};
